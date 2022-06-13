6868db8fb876c558c0247df0a376d3561582050d
Sellyme <s_lamerichs@hotmail.com>
2022-06-14 06:39:30 +0930

    Updating versioning
    minor version number 0.6.1 with support for Pokemon Go (gameid 2006)

--- CS-EnhancedTableLayouter.user.js
+++ CS-EnhancedTableLayouter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         CS-EnhancedTableLayouter
-// @version      0.6
+// @version      0.6.1
 // @description  Allow two dimensional score tables in Cyberscore games. Based on Kyu's CS-TableLayouter for Pokemon Snap
 // @author       Sellyme
 // @include      https://cyberscore.me.uk/game/1550
