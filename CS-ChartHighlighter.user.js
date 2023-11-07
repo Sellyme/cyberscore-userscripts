1c75e930198dcb655a0ec3595f48a5b786dda99d
Sellyme <s_lamerichs@hotmail.com>
2023-11-07 18:28:11 +1030

    adding memory of selected highlights on a per-game basis

--- CS-ChartHighlighter.user.js
+++ CS-ChartHighlighter.user.js
@@ -12,13 +12,17 @@
 // ==/UserScript==
 GM_addStyle(
 `
-#themeDiv {
+#highlightDiv {
 	float: right;
 	margin-right: 5px;
 }
 `
 );
 (function(){
+	const urlComponents = window.location.href.split("/")
+	const gameNum = parseInt(urlComponents[urlComponents.length - 1])
+	const basestyle = "tr.REPLACE_ME{background-color:rgb(17,81,17) !important;}";
+
 	//define helper functions for changing the selected highlight preference
 	function addStyle(style) {
 		var headEl = document.getElementsByTagName("head")[0];
@@ -28,70 +32,101 @@ GM_addStyle(
 		styleEl.appendChild(document.createTextNode(style));
 		headEl.appendChild(styleEl);
 	}
-	function changeTheme() {
-		var style = basestyle;
-		var theme = document.getElementById('themeSelect').value;
-		style = style.replace("none",theme);
+	function saveHighlight(value) {
+		const urlComponents = window.location.href.split("/")
+		const gameNum = parseInt(urlComponents[urlComponents.length - 1])
+		if(value == "none") { //for default
+			window.localStorage.removeItem("highlight-"+gameNum);
+		} else {
+			window.localStorage.setItem("highlight-"+gameNum, value);
+		}
+	}
+	function applyHighlight(save=true) { //pass in false to avoid saving this value for future use
+		var value = document.getElementById('highlightSelect').value;
 		var rules = document.getElementById('chartHighlightRules');
 		if (rules) {
 			rules.remove();
 		}
-		addStyle(style);
+		if(value != "none") {
+			var style = basestyle;
+			style = style.replace("REPLACE_ME",value);
+			addStyle(style);
+		}
+		if(save) {
+			saveHighlight(value);
+		}
 	}
 	//helper function to add highlight options to DOM
-	function addCustomHighlight(value, desc, sel = null) {
+	//value and desc are the attributes of the <option>, element is the <select>
+	//and loadValue is the localStorage entry determining the saved/default selected value
+	function addCustomHighlight(value, desc, loadValue, element) {
 		let newOpt = document.createElement('option');
 		newOpt.value = value;
 		newOpt.innerText = desc;
-		if(!sel) {
-			sel = document.getElementById('themeSelect');
+		if(value == loadValue) {
+			newOpt.selected = true;
 		}
-		sel.appendChild(newOpt);
+		if(!element) {
+			element = document.getElementById('highlightSelect');
+		}
+		element.appendChild(newOpt);
 	}
+
+	//default highlights
+	let highlights = [
+		{"value": "none", "desc": "None"},
+		{"value": "first", "desc": "Firsts"},
+		{"value": "submitted", "desc": "Submitted"},
+	]
 
 	//build the highlight selector
 	var div = document.createElement('div');
-	div.id = "themeDiv";
+	div.id = "highlightDiv";
 	var spanEl = document.createElement('span');
 	spanEl.innerText = "Highlight:";
 	div.appendChild(spanEl);
 	var selectEl = document.createElement('select');
-	selectEl.id = "themeSelect";
+	selectEl.id = "highlightSelect";
 	selectEl.classList.add('borderRadius');
-	selectEl.onchange = changeTheme;
-	addCustomHighlight("none","None",selectEl);
-	addCustomHighlight("first","Firsts",selectEl);
-	addCustomHighlight("submitted","Submitted",selectEl);
+	selectEl.onchange = applyHighlight;
+	//load whatever preset highlight exists, if any
+	let savedValue = window.localStorage.getItem("highlight-"+gameNum);
+	let defaultValue = window.localStorage.getItem("highlight-default"); //currently unimplemented, be careful when implementing not to let default selections write game-specific ones unnecessarily
+	let loadValue = savedValue || defaultValue || "none"; //default to "none"
+
+	for(var i = 0; i < highlights.length; i++) {
+		let highlight = highlights[i];
+		let selected =
+		addCustomHighlight(highlight.value, highlight.desc, loadValue, selectEl);
+	}
+
 	div.appendChild(selectEl);
-	var basestyle = "tr.none{background-color:rgb(17,81,17) !important;}";
 
 	//insert the selector
 	var pageleft = document.getElementById('pageleft');
 	pageleft.insertBefore(div, pageleft.firstChild);
-	addStyle(basestyle);
 
 	//individual game configs
-	function setupGame(gameNum) {
+	function setupGame(gameNum, loadValue) {
 		let tagFunction;
 		switch(gameNum) {
 			case 3225:
 				tagFunction = tagMelvor;
-				addCustomHighlight("melvor99", "Melvor 99s/120s");
+				addCustomHighlight("melvor99", "Melvor 99s/120s", loadValue);
 				break;
 			case 3231:
 				tagFunction = tagFinalBarLine;
-				addCustomHighlight("maxed", "9,999,999s");
+				addCustomHighlight("maxed", "9,999,999s", loadValue);
 				break;
 			case 3279:
 				tagFunction = tagPokeclicker;
-				addCustomHighlight("resisted", "EV Resisted")
+				addCustomHighlight("resisted", "EV Resisted", loadValue)
 				break;
 			default:
 				tagFunction = tagGeneric;
 		}
 		return tagFunction;
 	}
-
 
 	//this set of functions is called on any chart row if the user has a valid score on that chart
 	//the exact behaviour of how to tag it varies by game
@@ -136,9 +171,9 @@ GM_addStyle(
 		}
 	}
 
-	const urlComponents = window.location.href.split("/")
-	const gameNum = parseInt(urlComponents[urlComponents.length - 1])
-	let tagFunction = setupGame(gameNum);
+	//add in any game-specific highlights, set up the checking function to iterate, and apply the <style>
+	let tagFunction = setupGame(gameNum, loadValue);
+	applyHighlight(false); //we've just read the saved config out of localStorage, so no need to save it back
 
 	let groups;
 		//skip any enhancedTableLayout tables
